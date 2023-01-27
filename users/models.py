93e5c0d8d951c07aee3719719997fe14365738ad
P-Horus <feranmia51@gmail.com>
2023-01-27 22:25:55 +0100

    Work on Authentication

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20, null=True, blank=True)
+    first_name = models.CharField(max_length=200, null=True, blank=True)
+    second_name = models.CharField(max_length=200, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True) 
+    id = models.UUIDField(default=uuid.uuid4,unique=True, editable=False, primary_key=True)
+    
+    
+    def __str__(self):
+        return self.user
+    
+    
+
--- users/views.py
+++ users/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CustomField
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 def registerPage(request):
-    page = CustomField()
-    context = {'page':page}
+    # page = 'register'
+    form = CustomField()
+    if request.method == 'POST':
+        form = CustomField(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'You have been registered successfully')
+            return redirect('login')
+    context = {'form':form}
     return render(request, 'users/register.html', context)
+
+
+def loginPage(request):
+    page = 'login-page'
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    context = {'page':page}
+    return render(request, 'users/login.html', context)
+
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
